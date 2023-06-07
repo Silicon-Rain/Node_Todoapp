50b440e41c1e3f6bae1932ac4629e1cae0a022ff
AbhePing <abheshek916@gmail.com>
2023-06-07 08:24:22 +0530

    Made the api accessible in both development and production mode

--- server.js
+++ server.js
@@ -3,8 +3,6 @@ import { connectDB } from "./data/database.js";
 
 connectDB();
 
-console.log(process.env.PORT)
-
 app.listen(process.env.PORT, () => {
-    console.log("Server is working");
+    console.log(`Server is working on port: ${process.env.PORT} in ${process.env.NODE_ENV} mode`);
 });
